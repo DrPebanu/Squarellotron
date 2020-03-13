241c4c20fe33048db79a84f4bbf360cc8d0d56f6
HOME\Pebanu <38572963+DrPebanu@users.noreply.github.com>
2020-03-13 16:05:15 +0100

    Numbers now appear on all cubes and they instantiate at the right place

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -18,10 +18,17 @@ public class GameSetup : MonoBehaviour
     // Create array of gameobjects and instantiate prefabs
     void CreateGameBoard(int size_x, int size_y) {
         gameBoard = new GameObject[size_x, size_y];
-        for (int i = 0; i < size_x; i++){
-            for (int j = 0; j < size_y; j++)
+        int counter = 1;
+
+        for (int j = 0; j < size_x; j++){
+            for (int i = 0; i < size_y; i++)
             {
-                gameBoard[i, j] = (GameObject)Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                gameBoard[i, j] = (GameObject)Instantiate(tilePrefab, new Vector3(i, -j, 0), Quaternion.identity);
+                GameObject currentTile = gameBoard[i, j];
+                currentTile.GetComponent<TileData>().Nummer = counter;
+                currentTile.GetComponent<TileData>().SetNummerText();
+                counter += 1;
+
             }
         }
     }
---
+++ Assets/Scripts/TileData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour
+{
+    public int Nummer;
+
+    public void SetNummerText() {
+        this.GetComponentInChildren<TextMesh>().text = Nummer.ToString();
+
+    }
+}
